e73c94a5a38793ed1f652a5ab2f9a645b2300f1b
gguo <gguo@movoto.com>
2017-04-07 15:35:41 +0800

    updated

---
+++ pages/sitemap/sitemap.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://pi.movoto.com/p/100/ML81645659_0_eBUnJQ_t.jpeg',
+      'http://pi.movoto.com/p/100/ML81645659_0_Y2yVRj.jpeg',
+      'http://pi.movoto.com/p/100/ML81645659_0_fBrq7U.jpeg'
+    ]
+  }
+})
