0e9e0145dcd2d9c5ad6f1c4b8c32a0697b0f2d68
gguo <gguo@movoto.com>
2017-04-09 17:21:33 +0800

    1st

--- pages/sitemap/sitemap.js
+++ pages/sitemap/sitemap.js
@@ -1,9 +1,16 @@
 Page({
   data: {
+    userInput: '',
     imgUrls: [
       'http://pi.movoto.com/p/100/ML81645659_0_eBUnJQ_t.jpeg',
       'http://pi.movoto.com/p/100/ML81645659_0_Y2yVRj.jpeg',
       'http://pi.movoto.com/p/100/ML81645659_0_fBrq7U.jpeg'
     ]
+  },
+  updateInputText: function(e){
+    this.data.userInput = e.detail.value;
+  },
+  performSearch:function(e){
+    console.log(this.data.userInput)
   }
 })
