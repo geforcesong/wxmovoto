654f23184aa10a4cd62ddf9808b948f6ab50bbeb
gguo <gguo@movoto.com>
2017-04-10 17:02:18 +0800

    updated

--- pages/sitemap/sitemap.js
+++ pages/sitemap/sitemap.js
@@ -11,6 +11,19 @@ Page({
     this.data.userInput = e.detail.value;
   },
   performSearch:function(e){
-    console.log(this.data.userInput)
+    let data = {"conditions":{"isDistressed":0,"isFixerUpper":0,"hasPhoto":1,"searchPropertyStatus":"INACTIVE","pageIndex":1,"isOpenHousesOnly":0,"minPrice":0,"minBed":0,"searchType":"ZIPCODE","sortColumn":"PRICE","maxCountPerPage":50,"hasPool":0,"location":{"lat":40.7464969,"lng":-74.0094471},"input":"10011 New York NY","propertyTypes":["SINGLE_FAMILY_HOUSE","CONDO","MULTI_FAMILY","LAND","MOBILE","OTHER"],"minBath":0,"sortOrder":"DESC"}}
+
+    let url = "https://appapi.movoto.com/api/search";
+    wx.request({
+      url: url,
+      method: "POST",
+      data: data,
+      header: {
+          'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res.data)
+      }
+    })
   }
 })
