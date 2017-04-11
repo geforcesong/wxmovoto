270b4d6b0e5576dfe543d8c4fe524542878acdfd
gguo <gguo@movoto.com>
2017-04-11 17:29:54 +0800

    updated

--- pages/sitemap/sitemap.js
+++ pages/sitemap/sitemap.js
@@ -1,17 +1,24 @@
 Page({
   data: {
+    title: 'hello, world',
     userInput: '',
-    imgUrls: [
-      'http://pi.movoto.com/p/100/ML81645659_0_eBUnJQ_t.jpeg',
-      'http://pi.movoto.com/p/100/ML81645659_0_Y2yVRj.jpeg',
-      'http://pi.movoto.com/p/100/ML81645659_0_fBrq7U.jpeg'
+    listings: [
+      { "name": "George", "Age": 20 },
+      { "name": "Hust", "Age": 30 }
     ]
   },
-  updateInputText: function(e){
+  updateInputText: function (e) {
     this.data.userInput = e.detail.value;
+    this.data.listings.push({ "name": "Hust1", "Age": 40 });
+    this.setData({
+      listings:this.data.listings
+    })
+    this.setData({
+      title:'hello earth'
+    })
   },
-  performSearch:function(e){
-    let data = {"conditions":{"isDistressed":0,"isFixerUpper":0,"hasPhoto":1,"searchPropertyStatus":"INACTIVE","pageIndex":1,"isOpenHousesOnly":0,"minPrice":0,"minBed":0,"searchType":"ZIPCODE","sortColumn":"PRICE","maxCountPerPage":50,"hasPool":0,"location":{"lat":40.7464969,"lng":-74.0094471},"input":"10011 New York NY","propertyTypes":["SINGLE_FAMILY_HOUSE","CONDO","MULTI_FAMILY","LAND","MOBILE","OTHER"],"minBath":0,"sortOrder":"DESC"}}
+  performSearch: function (e) {
+    let data = { "conditions": { "isDistressed": 0, "isFixerUpper": 0, "hasPhoto": 1, "searchPropertyStatus": "INACTIVE", "pageIndex": 1, "isOpenHousesOnly": 0, "minPrice": 0, "minBed": 0, "searchType": "ZIPCODE", "sortColumn": "PRICE", "maxCountPerPage": 50, "hasPool": 0, "location": { "lat": 40.7464969, "lng": -74.0094471 }, "input": "10011 New York NY", "propertyTypes": ["SINGLE_FAMILY_HOUSE", "CONDO", "MULTI_FAMILY", "LAND", "MOBILE", "OTHER"], "minBath": 0, "sortOrder": "DESC" } }
 
     let url = "https://appapi.movoto.com/api/search";
     wx.request({
@@ -19,9 +26,9 @@ Page({
       method: "POST",
       data: data,
       header: {
-          'content-type': 'application/json'
+        'content-type': 'application/json'
       },
-      success: function(res) {
+      success: function (res) {
         console.log(res.data)
       }
     })
