11008b733366d6e7f353866c9affb9f04b585ae8
gguo <gguo@movoto.com>
2017-04-13 12:19:57 +0800

    updated

--- pages/sitemap/sitemap.js
+++ pages/sitemap/sitemap.js
@@ -3,21 +3,25 @@ var PropertyFactory = require('../../factories/propertyFactory.js');
 Page({
   data: {
     userInput: '',
-    listings: []
+    listings: [],
+    hiddenLoading: true
   },
   updateInputText: function (e) {
     this.data.userInput = e.detail.value;
   },
-  onLoad:function(e){
+  onLoad: function (e) {
     this.performSearch();
   },
   performSearch: function (e) {
     let propertyFactory = new PropertyFactory();
     var self = this;
     var options = {};
-    if(this.data.userInput){
+    if (this.data.userInput) {
       options.input = this.data.userInput;
     }
+    this.setData({
+      hiddenLoading: false
+    })
     propertyFactory.search(options).then((ret) => {
       if (ret && ret.status && ret.status.code === 0) {
         let listings = ret.data.listings;
@@ -25,6 +29,9 @@ Page({
           listings: listings
         })
       }
+      this.setData({
+        hiddenLoading: true
+      })
     });
   }
 })
