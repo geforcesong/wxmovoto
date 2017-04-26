9b8dd994fa28e41b78278c1ca3138a0cde0bdac5
gguo <gguo@movoto.com>
2017-04-26 14:49:45 +0800

    added hide loading

--- pages/dpp/dpp.js
+++ pages/dpp/dpp.js
@@ -4,34 +4,52 @@ Page({
     data: {
         url: '',
         images: [],
-        property: null
+        property: null,
+        disclaimer: '',
+        hiddenLoading: true
     },
     onLoad: function (query) {
         // if (!query || !query.url)
         //     throw new Error('the url is invalid');
-        //this.setData({ url: query.url });
+        // this.setData({ url: query.url });
         this.setData({ url: 'unincorporated-ca/100_80248785/for-sale/' });
+        this.setData({ hiddenLoading: false });
         let propertyFactory = new PropertyFactory();
         propertyFactory.getProperty(this.data.url).then((property) => {
-            console.log(property)
             if (!property) {
                 throw new Error('The property does not exist.')
             }
-            this.setData({images: this.populateImage(property)});
-            this.setData({property: property});
+            this.setData({ images: this.populateImage(property) });
+            this.setData({ property: property });
+            this.poppulateDisclaimer(property);
+            this.setData({ hiddenLoading: true });
         }).catch((err) => {
+            this.setData({ hiddenLoading: true });
             app.redirectToError(err);
         })
     },
-    populateImage(property){
+    populateImage(property) {
         let images = [];
-        if(property){
-            if(property.imagesURL && property.imagesURL.length){
+        if (property) {
+            if (property.imagesURL && property.imagesURL.length) {
                 images = images.concat(property.imagesURL);
-            } else{
+            } else {
                 images.push(property.tnImgPath);
             }
         }
         return images;
+    },
+    poppulateDisclaimer(property) {
+        let disclaimer = property.mls.disclaimer;
+        var date = new Date();
+        disclaimer = disclaimer.replace("[CURRENT_YEAR]", date.getFullYear());
+        this.setData({ disclaimer: disclaimer });
+    },
+    onCardTap(e) {
+        if (e && e.currentTarget && e.currentTarget.dataset && e.currentTarget.dataset.url) {
+            wx.navigateTo({
+                url: `../dpp/dpp?url=${e.currentTarget.dataset.url}`
+            })
+        }
     }
 })
